ea92c49da21a2084d53df102b144271f2f1f4af3
Chang Shi <changshi.lim@databricks.com>
2026-03-09 12:28:34 +0800

    modified uc function script

--- lab_setup/scripts/uc_functions/credit_risk.sql
+++ lab_setup/scripts/uc_functions/credit_risk.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE FUNCTION {catalog_name}.{schema_name}.check_credit_risk (
-  customer_id STRING COMMENT 'The customer ID'
+  p_customer_id STRING COMMENT 'The customer ID'
 )
 RETURNS STRING
 COMMENT 'Retrieves credit risk for a customer. Example usage: SELECT {{function_name}}("CUST0003")'
 RETURN
   SELECT to_json(struct(customer_id, credit_score, risk_category, probability_of_default)) AS json_data
   FROM {catalog_name}.{schema_name}.apac_risk_profiles
-  WHERE customer_id = customer_id
+  WHERE customer_id = p_customer_id
   LIMIT 1
